fff6893db25c1d58f94fb06af830d706446abbec
Smushed <smushedcode@gmail.com>
2018-10-10 22:51:37 -0500

    Initializing the HTML

---
+++ assets/app.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyC0bFCK4F0yPrSCT4HOAaBjbW4zze4Nbos",
+    authDomain: "train-schedule-smushed.firebaseapp.com",
+    databaseURL: "https://train-schedule-smushed.firebaseio.com",
+    projectId: "train-schedule-smushed",
+    storageBucket: "train-schedule-smushed.appspot.com",
+    messagingSenderId: "70873716006"
+  };
+  firebase.initializeApp(config);
