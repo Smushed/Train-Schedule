6168ad4595102c9c767d1adc31ae73f0caad0c37
Smushed <smushedcode@gmail.com>
2018-10-12 19:26:25 -0500

    Still working, just the outline for now

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,39 @@
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyC0bFCK4F0yPrSCT4HOAaBjbW4zze4Nbos",
-    authDomain: "train-schedule-smushed.firebaseapp.com",
-    databaseURL: "https://train-schedule-smushed.firebaseio.com",
-    projectId: "train-schedule-smushed",
-    storageBucket: "train-schedule-smushed.appspot.com",
-    messagingSenderId: "70873716006"
-  };
-  firebase.initializeApp(config);
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyC0bFCK4F0yPrSCT4HOAaBjbW4zze4Nbos",
+  authDomain: "train-schedule-smushed.firebaseapp.com",
+  databaseURL: "https://train-schedule-smushed.firebaseio.com",
+  projectId: "train-schedule-smushed",
+  storageBucket: "train-schedule-smushed.appspot.com",
+  messagingSenderId: "70873716006"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+var trainSchedule = {
+  milwaukee: {
+    name: "Hiawatha Line",
+    destination: "Milwaukee, WI",
+    frequency: 90,
+    startTime: 240,
+    endTime: 1380,
+    departTimes: [],
+    nextArrival: 0,
+    minutesAway: 0,
+  },
+  portland: {
+    name: "Oregon Trail",
+    destination: "Portland, OR",
+    frequency: 90,
+    nextArrival: 0,
+    minutesAway: 0,
+  },
+  philadelphia: {
+    name: "Rocky Train",
+    destination: "Philadelphia, PA",
+    frequency: 90,
+    nextArrival: 0,
+    minutesAway: 0,
+  }
+}
